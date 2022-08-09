d4b2c44344c2465258cfb356dff91b2ee2579498
Murat Yıldız <murat.yildiz@kboxglobal.com>
2022-08-09 23:15:35 +0300

    refactor: add json property for data fields

--- src/main/java/com/github/yildizmy/dto/request/EmployeeRequest.java
+++ src/main/java/com/github/yildizmy/dto/request/EmployeeRequest.java
@@ -1,6 +1,7 @@
 package com.github.yildizmy.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,12 +16,20 @@ import static com.github.yildizmy.common.Constants.DATE_PATTERN;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeRequest {
+
+    @JsonProperty("first_name")
     private String firstName;
+
+    @JsonProperty("last_name")
     private String lastName;
+
+    @JsonProperty("email")
     private String email;
+
+    @JsonProperty("country")
     private String country;
 
-    // @JsonFormat has nothing to do with "displaying", it is for reading/writing from/to JSON
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_PATTERN)
+    @JsonProperty("date_of_birth")
     private LocalDate dateOfBirth;
 }
