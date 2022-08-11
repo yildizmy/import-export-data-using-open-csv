600bfb523ea60eee83842e910cc4f6341c041bc6
Murat Yıldız <murat.yildiz@kboxglobal.com>
2022-08-11 19:30:57 +0300

    feat: add import method

--- src/main/java/com/github/yildizmy/util/CsvHelper.java
+++ src/main/java/com/github/yildizmy/util/CsvHelper.java
@@ -1,13 +1,14 @@
 package com.github.yildizmy.util;
 
+import com.github.yildizmy.dto.request.EmployeeRequest;
 import com.github.yildizmy.dto.response.EmployeeDto;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.bean.*;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.github.yildizmy.common.Constants.FAILED_TO_IMPORT;
@@ -15,6 +16,48 @@ import static com.opencsv.ICSVWriter.*;
 
 @SuppressWarnings("java:S1118")
 public class CsvHelper {
+
+    public static List<EmployeeRequest> importFromCsv(String fileName) {
+        List<EmployeeRequest> employees = new ArrayList<>();
+        try {
+            // create a reader
+            InputStream inputStream = new ClassPathResource("data/" + fileName).getInputStream();
+            Reader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            // columns name
+            String[] columns = {"id", "firstName", "lastName", "email", "country", "dateOfBirth"};
+
+            // create a mapping strategy
+            ColumnPositionMappingStrategy strategy = new ColumnPositionMappingStrategy();
+            strategy.setType(EmployeeRequest.class);
+            strategy.setColumnMapping(columns);
+
+            // create csv bean reader
+            CsvToBean csvToBean = new CsvToBeanBuilder(reader)
+                    .withMappingStrategy(strategy)
+                    .withSkipLines(1)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            // iterate through employees
+            for (EmployeeRequest request : (Iterable<EmployeeRequest>) csvToBean) {
+                EmployeeRequest employee = new EmployeeRequest(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getCountry(),
+                        request.getDateOfBirth()
+                );
+                employees.add(employee);
+            }
+            // close the reader
+            reader.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return employees;
+    }
 
     public static void exportToCsv(PrintWriter writer, List<EmployeeDto> employees) throws IOException {
         try {
