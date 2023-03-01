f836baafb899057bb90b590e413b14b41a8aace4
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-02 01:16:47 +0300

    refactor: remove unused variable

--- src/main/java/com/github/yildizmy/common/Constants.java
+++ src/main/java/com/github/yildizmy/common/Constants.java
@@ -2,7 +2,8 @@ package com.github.yildizmy.common;
 
 public final class Constants {
 
-    private Constants() {}
+    private Constants() {
+    }
 
     public static final String TRACE = "trace";
     public static final String CONTENT_TYPE = "text/csv";
@@ -11,10 +12,9 @@ public final class Constants {
 
     public static final String SUCCESS = "Success";
     public static final String SUCCESSFULLY_CREATED = "Employee(s) created successfully";
-    public static final String SUCCESSFULLY_DELETED = "Employee(s) deleted successfully";
     public static final String VALIDATION_ERROR = "Validation error. Check 'errors' field for details";
     public static final String ENTITY_NOT_FOUND = "Item not found";
     public static final String UNKNOWN_ERROR = "Unknown error occurred";
     public static final String FAILED_TO_IMPORT = "Failed to import data to csv file: ";
-    public static final String MAX_UPLOAD_SIZE_EXCEEDED="Maximum upload size exceeded";
+    public static final String MAX_UPLOAD_SIZE_EXCEEDED = "Maximum upload size exceeded";
 }
