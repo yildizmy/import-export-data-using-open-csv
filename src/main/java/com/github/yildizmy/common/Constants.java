d33b9f4140b618bafbdbfd1db30b3cfd72a11117
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-03-02 00:57:42 +0300

    refactor: make class final

--- src/main/java/com/github/yildizmy/common/Constants.java
+++ src/main/java/com/github/yildizmy/common/Constants.java
@@ -1,14 +1,14 @@
 package com.github.yildizmy.common;
 
-@SuppressWarnings("java:S1118")
-public class Constants {
+public final class Constants {
+
+    private Constants() {}
 
     public static final String TRACE = "trace";
     public static final String CONTENT_TYPE = "text/csv";
     public static final String DATE_PATTERN_FOR_EXPORT = "dd/MM/yyyy";
     public static final String DATE_PATTERN_FOR_IMPORT = "yyyy-MM-dd";
 
-    // TODO: Move to message.properties file after applying i18n
     public static final String SUCCESS = "Success";
     public static final String SUCCESSFULLY_CREATED = "Employee(s) created successfully";
     public static final String SUCCESSFULLY_DELETED = "Employee(s) deleted successfully";
